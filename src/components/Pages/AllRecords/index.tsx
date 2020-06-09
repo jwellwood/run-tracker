743d1f8ee9a0f498cc2747aa22ee01e35cdb982d
N W <nw@Ns-MacBook-Pro.local>
2020-06-09 14:41:54 +0200

    added routes and page component indexes

---
+++ src/components/Pages/AllRecords/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AllRecords = () => {
+  return <div></div>;
+};
+
+export default AllRecords;
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,8 +1,28 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { HOME, ABOUT } from '.';
+import {
+  HOME,
+  ABOUT,
+  SIGN_IN,
+  REGISTER,
+  RESET_PASSWORD,
+  PROFILE,
+  EDIT_PROFILE,
+  ALL_RECORDS,
+  RECORD,
+  ADD_RECORD,
+  EDIT_RECORD,
+} from '.';
 const Home = lazy(() => import('../components/Home'));
 const About = lazy(() => import('../components/About'));
+const Register = lazy(() => import('../components/Pages/Register'));
+const ResetPassword = lazy(() => import('../components/Pages/ResetPassword'));
+const Profile = lazy(() => import('../components/Pages/Profile'));
+const EditProfile = lazy(() => import('../components/Pages/EditProfile'));
+const AllRecords = lazy(() => import('../components/Pages/AllRecords'));
+const Record = lazy(() => import('../components/Pages/Record'));
+const AddRecord = lazy(() => import('../components/Pages/AddRecord'));
+const EditRecord = lazy(() => import('../components/Pages/EditRecord'));
 
 function AppRoutes() {
   return (
@@ -11,6 +31,14 @@ function AppRoutes() {
         <Suspense fallback={<div style={{ color: 'red' }}>Loading...</div>}>
           <Route exact path={HOME} component={Home} />
           <Route exact path={ABOUT} component={About} />
+          <Route exact path={REGISTER} component={Register} />
+          <Route exact path={RESET_PASSWORD} component={ResetPassword} />
+          <Route exact path={PROFILE} component={Profile} />
+          <Route exact path={EDIT_PROFILE} component={EditProfile} />
+          <Route exact path={ALL_RECORDS} component={AllRecords} />
+          <Route exact path={RECORD} component={Record} />
+          <Route exact path={ADD_RECORD} component={AddRecord} />
+          <Route exact path={EDIT_RECORD} component={EditRecord} />
         </Suspense>
       </Switch>
     </Router>
